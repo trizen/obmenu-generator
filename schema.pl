0984b3c1fb4ab8b4d728d1b99a53cb1c1a9550bf
trizen <trizenx@gmail.com>
2015-06-02 22:09:14 +0300

    - Performance improvements. (from ~21ms to ~14ms) - Improved the encoding of XML special characters. - The underscore character (`_`) is now escaped correctly. - Added XML <![CDATA[]]> for the execution command, which allows any exotic characters to exist inside a given command.

--- schema.pl
+++ schema.pl
@@ -27,14 +27,14 @@ require "$ENV{HOME}/.config/obmenu-generator/config.pl";
 my $editor = $CONFIG->{editor};
 
 our $SCHEMA = [
+
     #          COMMAND                 LABEL                ICON
     {item => ['xdg-open .',        'File Manager',      'file-manager']},
     {item => ['xterm',             'Terminal',          'terminal']},
     {item => ['xdg-open http://',  'Web Browser',       'web-browser']},
     {item => ['gmrun',             'Run command',       'system-run']},
-    {item => ['pidgin',            'Instant messaging', 'system-users']},
 
-    {sep => 'Applications'},
+    {sep => 'Categories'},
 
     #          NAME            LABEL                ICON
     {cat => ['utility',     'Accessories', 'applications-utilities']},
@@ -73,38 +73,32 @@ our $SCHEMA = [
     {begin_cat => ['Advanced Settings', 'gnome-settings']},
 
         # Configuration files
-        {item      => ["$editor ~/.conkyrc",              'Conky RC',    $editor]},
-        {item      => ["$editor ~/.config/tint2/tint2rc", 'Tint2 Panel', $editor]},
+        {item => ["$editor ~/.conkyrc",              'Conky RC',    'text-x-source']},
+        {item => ["$editor ~/.config/tint2/tint2rc", 'Tint2 Panel', 'text-x-source']},
 
         # obmenu-generator category
         {begin_cat => ['Obmenu-Generator', 'menu-editor']},
-            {item      => ["$editor ~/.config/obmenu-generator/schema.pl", 'Menu Schema', $editor]},
-            {item      => ["$editor ~/.config/obmenu-generator/config.pl", 'Menu Config', $editor]},
+            {item => ["$editor ~/.config/obmenu-generator/schema.pl", 'Menu Schema', 'text-x-source']},
+            {item => ["$editor ~/.config/obmenu-generator/config.pl", 'Menu Config', 'text-x-source']},
 
             {sep  => undef},
-            {item => ['obmenu-generator -p',    'Generate a pipe menu',              'menu-editor']},
-            {item => ['obmenu-generator -s',    'Generate a static menu',            'menu-editor']},
-            {item => ['obmenu-generator -p -i', 'Generate a pipe menu with icons',   'menu-editor']},
-            {item => ['obmenu-generator -s -i', 'Generate a static menu with icons', 'menu-editor']},
+            {item => ["$0 -p",       'Generate a pipe menu',              'menu-editor']},
+            {item => ["$0 -s -c",    'Generate a static menu',            'menu-editor']},
+            {item => ["$0 -p -i",    'Generate a pipe menu with icons',   'menu-editor']},
+            {item => ["$0 -s -i -c", 'Generate a static menu with icons', 'menu-editor']},
             {sep  => undef},
 
-            {item    => ['obmenu-generator -d', 'Refresh Icon Set', 'gtk-refresh']},
+            {item => ['obmenu-generator -d', 'Refresh Icon Set', 'gtk-refresh']},
         {end_cat => undef},
 
         # Openbox category
         {begin_cat => ['Openbox', 'openbox']},
-            {item      => ['openbox --reconfigure',               'Reconfigure Openbox', 'openbox']},
-            {item      => ["$editor ~/.config/openbox/autostart", 'Openbox Autostart',    $editor]},
-            {item      => ["$editor ~/.config/openbox/rc.xml",    'Openbox RC',           $editor]},
-            {item      => ["$editor ~/.config/openbox/menu.xml",  'Openbox Menu',         $editor]},
+            {item => ['openbox --reconfigure',               'Reconfigure Openbox', 'openbox']},
+            {item => ["$editor ~/.config/openbox/autostart", 'Openbox Autostart',   'shellscript']},
+            {item => ["$editor ~/.config/openbox/rc.xml",    'Openbox RC',          'text-xml']},
+            {item => ["$editor ~/.config/openbox/menu.xml",  'Openbox Menu',        'text-xml']},
         {end_cat => undef},
 
-        # Pacman category
-        {begin_cat => ['Pacman / Servers', 'package-manager-icon']},
-            {item      => ["gksu $editor /etc/makepkg.conf",        'Makepkg Config',    'start-here']},
-            {item      => ["gksu $editor /etc/pacman.conf",         'Pacman Config',     'start-here']},
-            {item      => ["gksu $editor /etc/pacman.d/mirrorlist", 'Pacman Mirrorlist', 'start-here']},
-        {end_cat => undef},
     {end_cat => undef},
     {sep => undef},
 
@@ -113,4 +107,7 @@ our $SCHEMA = [
 
     # This option uses the default Openbox's action "Exit"
     {exit => ['Exit', 'exit']},
+
+    # This uses the 'oblogout' menu
+    # {item => ['oblogout', 'Exit', 'exit']},
 ]
