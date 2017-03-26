3085073330aa5131ae2d5e786d528e1fd3f5d083
trizen <trizenx@gmail.com>
2017-03-26 05:54:49 +0300

    - The generic schema entry `obgenmenu` now includes icons when `-i` is specified. - Option `-d` also removes the cache.db file. - Updated the icon names in schema.pl

--- schema.pl
+++ schema.pl
@@ -29,11 +29,11 @@ my $editor = $CONFIG->{editor};
 
 our $SCHEMA = [
 
-    #          COMMAND                 LABEL                ICON
-    {item => ['xdg-open .',        'File Manager',      'file-manager']},
-    {item => ['xterm',             'Terminal',          'terminal']},
-    {item => ['xdg-open http://',  'Web Browser',       'web-browser']},
-    {item => ['gmrun',             'Run command',       'system-run']},
+    #          COMMAND                 LABEL              ICON
+    {item => ['xdg-open .',       'File Manager', 'system-file-manager']},
+    {item => ['xterm',            'Terminal',     'utilities-terminal']},
+    {item => ['xdg-open http://', 'Web Browser',  'web-browser']},
+    {item => ['gmrun',            'Run command',  'system-run']},
 
     {sep => 'Categories'},
 
@@ -50,12 +50,6 @@ our $SCHEMA = [
     {cat => ['settings',    'Settings',    'applications-accessories']},
     {cat => ['system',      'System',      'applications-system']},
 
-    #{cat => ['qt',          'QT Applications',    'qt4logo']},
-    #{cat => ['gtk',         'GTK Applications',   'gnome-applications']},
-    #{cat => ['x_xfce',      'XFCE Applications',  'applications-other']},
-    #{cat => ['gnome',       'GNOME Applications', 'gnome-applications']},
-    #{cat => ['consoleonly', 'CLI Applications',   'applications-utilities']},
-
     #                  LABEL          ICON
     #{begin_cat => ['My category',  'cat-icon']},
     #             ... some items ...
@@ -66,49 +60,49 @@ our $SCHEMA = [
 
     ## Generic advanced settings
     #{sep       => undef},
-    #{obgenmenu => ['Openbox Settings', 'openbox']},
+    #{obgenmenu => ['Openbox Settings', 'applications-engineering']},
     #{sep       => undef},
 
     ## Custom advanced settings
-    {sep => undef},
-    {begin_cat => ['Advanced Settings', 'gnome-settings']},
+    {sep       => undef},
+    {begin_cat => ['Advanced Settings', 'applications-engineering']},
 
-        # Configuration files
-        {item => ["$editor ~/.conkyrc",              'Conky RC',    'text-x-source']},
-        {item => ["$editor ~/.config/tint2/tint2rc", 'Tint2 Panel', 'text-x-source']},
+      # Configuration files
+      {item => ["$editor ~/.conkyrc",              'Conky RC',    'text-x-generic']},
+      {item => ["$editor ~/.config/tint2/tint2rc", 'Tint2 Panel', 'text-x-generic']},
 
-        # obmenu-generator category
-        {begin_cat => ['Obmenu-Generator', 'menu-editor']},
-            {item => ["$editor ~/.config/obmenu-generator/schema.pl", 'Menu Schema', 'text-x-source']},
-            {item => ["$editor ~/.config/obmenu-generator/config.pl", 'Menu Config', 'text-x-source']},
+      # obmenu-generator category
+      {begin_cat => ['Obmenu-Generator', 'accessories-text-editor']},
+        {item      => ["$editor ~/.config/obmenu-generator/schema.pl", 'Menu Schema', 'text-x-generic']},
+        {item      => ["$editor ~/.config/obmenu-generator/config.pl", 'Menu Config', 'text-x-generic']},
 
-            {sep  => undef},
-            {item => ['obmenu-generator -s -c',    'Generate a static menu',            'menu-editor']},
-            {item => ['obmenu-generator -s -i -c', 'Generate a static menu with icons', 'menu-editor']},
-            {sep  => undef},
-            {item => ['obmenu-generator -p',       'Generate a dynamic menu',           'menu-editor']},
-            {item => ['obmenu-generator -p -i',    'Generate a dynamic menu with icons','menu-editor']},
-            {sep  => undef},
+        {sep  => undef},
+        {item => ['obmenu-generator -s -c',    'Generate a static menu',             'accessories-text-editor']},
+        {item => ['obmenu-generator -s -i -c', 'Generate a static menu with icons',  'accessories-text-editor']},
+        {sep  => undef},
+        {item => ['obmenu-generator -p',       'Generate a dynamic menu',            'accessories-text-editor']},
+        {item => ['obmenu-generator -p -i',    'Generate a dynamic menu with icons', 'accessories-text-editor']},
+        {sep  => undef},
 
-            {item => ['obmenu-generator -d', 'Refresh Icon Set', 'gtk-refresh']},
-        {end_cat => undef},
+        {item    => ['obmenu-generator -d', 'Refresh Icon Set', 'view-refresh']},
+      {end_cat => undef},
 
-        # Openbox category
-        {begin_cat => ['Openbox', 'openbox']},
-            {item => ['openbox --reconfigure',               'Reconfigure Openbox', 'openbox']},
-            {item => ["$editor ~/.config/openbox/autostart", 'Openbox Autostart',   'shellscript']},
-            {item => ["$editor ~/.config/openbox/rc.xml",    'Openbox RC',          'text-xml']},
-            {item => ["$editor ~/.config/openbox/menu.xml",  'Openbox Menu',        'text-xml']},
-        {end_cat => undef},
-
+      # Openbox category
+      {begin_cat => ['Openbox', 'openbox']},
+        {item      => ["$editor ~/.config/openbox/autostart", 'Openbox Autostart',   'text-x-generic']},
+        {item      => ["$editor ~/.config/openbox/rc.xml",    'Openbox RC',          'text-x-generic']},
+        {item      => ["$editor ~/.config/openbox/menu.xml",  'Openbox Menu',        'text-x-generic']},
+        {item      => ['openbox --reconfigure',               'Reconfigure Openbox', 'openbox']},
+      {end_cat => undef},
     {end_cat => undef},
+
     {sep => undef},
 
     ## The xscreensaver lock command
-    {item => ['xscreensaver-command -lock', 'Lock', 'lock']},
+    {item => ['xscreensaver-command -lock', 'Lock', 'system-lock-screen']},
 
     ## This option uses the default Openbox's action "Exit"
-    {exit => ['Exit', 'exit']},
+    {exit => ['Exit', 'application-exit']},
 
     ## This uses the 'oblogout' menu
     # {item => ['oblogout', 'Exit', 'exit']},
